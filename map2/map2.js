2943efec22b7b3900f82d73a5d7df6af50605b04
User1 <user@example.com>
2020-04-21 21:00:59 -0500

    Lab 8 Final

--- map2/map2.js
+++ map2/map2.js
@@ -1,5 +1,5 @@
-var mapOfPA = L.map('mapid').setView([40.915608, -77.591773],7.4);
-L.tileLayer('https://{s}.tile.thunderforest.com/transport-dark/{z}/{x}/{y}.png').addTo(mapOfPA)
+var mapOfPA = L.map('mapid2').setView([40.915608, -77.591773],7.4);
+L.tileLayer('https://{s}.tile.thunderforest.com/pioneer/{z}/{x}/{y}.png').addTo(mapOfPA)
 L.tileLayer.wms('http://mesonet.agron.iastate.edu/cgi-bin/wms/nexrad/n0r.cgi', {
   layers: 'nexrad-n0r-900913',
   format: 'image/png',
@@ -8,4 +8,11 @@ L.tileLayer.wms('http://mesonet.agron.iastate.edu/cgi-bin/wms/nexrad/n0r.cgi', {
 }).addTo(mapOfPA)
 L.esri.dynamicMapLayer({
   url: 'https://nowcoast.noaa.gov/arcgis/rest/services/nowcoast/radar_meteo_imagery_nexrad_time/MapServer'
+}).addTo(mapOfPA)
+
+L.tileLayer.wms('https://mesonet.agron.iastate.edu/cgi-bin/wms/us/wwa.cgi', {
+  layers: 'warnings_c',
+  format: 'image/png',
+  transparent: true,
+  attribution: 'NOAA, Iowa State University'
 }).addTo(mapOfPA)
